e494526463f86e5a0fed9bd92a8018802a86b630
pneyman <pneyman@bricato.com>
2012-04-09 15:14:11 -0700

    Added [super dealloc] to GMGridView.m

--- GMGridView/GMGridView.m
+++ GMGridView/GMGridView.m
@@ -266,6 +266,7 @@ static const UIViewAnimationOptions kDefaultAnimationOptions = UIViewAnimationOp
 {
     [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationDidReceiveMemoryWarningNotification object:nil];
     [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationWillChangeStatusBarOrientationNotification object:nil];
+    [super dealloc];
 }
 
 //////////////////////////////////////////////////////////////
